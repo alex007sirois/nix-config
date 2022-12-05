a8c916696ceec627f69ed56c544e5c841f3e775c
Alex Sirois <asirois@dimonoff.com>
2022-12-05 12:48:29 -0500

    feat: add git aliases

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -14,6 +14,14 @@
 
   programs.git = {
     enable = true;
+    aliases = {
+      aliases = "config --get-regexp ^alias\.";
+      branches = "branch -a  --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:blue)(%(committerdate:short)) [%(authorname)]' --sort=-committerdate";
+      logs = "log --pretty=format:'%C(auto)%h%C(reset) %C(cyan)%ad%C(auto)%d%C(reset) %s %C(blue)[%cn]%C(reset)' --date=short-local";
+      save = "!git add -A && git commit -m 'chore: savepoint'";
+      uncommit = "reset --mixed HEAD~1";
+      wipe = "!git reset --hard && git clean -fdx";
+    };
     lfs = {
       enable = true;
     };
