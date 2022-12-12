aacb34d8f5c57912a1525c7228a9ca65fe8e5990
Alex Sirois <asirois@dimonoff.com>
2022-12-11 22:29:33 -0500

    feat: use optimus driver for nvidia

--- nixos/modules/desktop-environment.nix
+++ nixos/modules/desktop-environment.nix
@@ -1,7 +1,10 @@
 { ... }: {
   services.xserver = {
     enable = true;
-    displayManager.gdm.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = false;
+    };
     desktopManager.gnome.enable = true;
   };
 }
