cfd53ea672ac5bf8a0cd44b662c1febf83c7c0c4
Alex Sirois <asirois@dimonoff.com>
2022-12-14 13:48:47 -0500

    feat: add support for emoji

--- nixos/modules/localisation.nix
+++ nixos/modules/localisation.nix
@@ -1,9 +1,29 @@
-{...}: {
+{pkgs, ...}: {
   time.timeZone = "America/Toronto";
-  i18n.defaultLocale = "en_CA.UTF-8";
   services.xserver = {
     layout = "ca";
     xkbVariant = "";
   };
   console.keyMap = "cf";
+
+  i18n = {
+    defaultLocale = "en_CA.UTF-8";
+    inputMethod = {
+      enabled = "ibus";
+      ibus.engines = [
+        (
+          pkgs.ibus-engines.typing-booster.override
+          {langs = ["en-ca" "fr-moderne"];}
+        )
+      ];
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    hunspell
+    hunspellDicts.en-ca
+    hunspellDicts.fr-moderne
+  ];
+
+  fonts.fonts = [pkgs.noto-fonts-emoji];
 }
