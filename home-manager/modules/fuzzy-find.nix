d68a5a15b576713729a5a552d24f507e205cfc5f
Alex Sirois <asirois@dimonoff.com>
2022-12-16 11:27:55 -0500

    feat: change pistol config to use assiciations format

--- home-manager/modules/fuzzy-find.nix
+++ home-manager/modules/fuzzy-find.nix
@@ -20,30 +20,77 @@
     changeDirWidgetCommand = "fd --follow --type directory";
   };
 
-  programs.pistol = {
-    enable = true;
-    config = {
-      "application/json" = "bat --paging=never --color=always %pistol-filename%";
-      "application/pdf" = "pdf2txt --maxpages 2 %pistol-filename%";
-      "application/x-x509-ca-cert" = "openssl x509 -text -noout -passin pass: -in %pistol-filename%";
-      "application/epub" = "pandoc -t plain %pistol-filename%";
-      "application/vnd.*document*" = "pandoc -t plain %pistol-filename%";
-      # FIXME https://github.com/nix-community/home-manager/issues/3486
-      # "application/*" = "hexyl %pistol-filename%";
-      "fpath .*\.log(.\d\w+)?(.gz)?$" = "lnav -n %pistol-filename%";
-      "fpath .*\.pem$" = "sh: openssl x509 -text -noout -passin pass: -in %pistol-filename% || bat %pistol-filename%";
-      "fpath .*\.done$" = "file -bi %pistol-filename%";
-      "audio/*" = "ffprobe -hide_banner  %pistol-filename%";
-      "image/*" = "chafa %pistol-filename%";
-      "inode/directory" = "exa --classify --group-directories-first --icons --time-style=long-iso %pistol-filename%";
-      "text/*" = "bat --paging=never --color=always %pistol-filename%";
-      "text/html" = "pandoc -t plain %pistol-filename%";
-      "text/x-tex" = "pandoc -t plain %pistol-filename%";
-      "video/*" = "ffprobe -hide_banner  %pistol-filename%";
-    };
-  };
-
   home.shellAliases = {
     fzf = "fzf --preview 'pistol {}'";
+  };
+
+  programs.pistol = {
+    enable = true;
+    associations = [
+      {
+        mime = "inode/directory";
+        command = "exa --classify --group-directories-first --icons --time-style=long-iso %pistol-filename%";
+      }
+      {
+        mime = "application/x-x509-ca-cert";
+        command = "openssl x509 -text -noout -passin pass: -in %pistol-filename%";
+      }
+      {
+        fpath = ".*\.pem$";
+        command = "sh: openssl x509 -text -noout -passin pass: -in %pistol-filename% || bat %pistol-filename%";
+      }
+      {
+        mime = "application/pdf";
+        command = "pdf2txt --maxpages 2 %pistol-filename%";
+      }
+      {
+        mime = "application/epub";
+        command = "pandoc -t plain %pistol-filename%";
+      }
+      {
+        mime = "application/vnd.*document*";
+        command = "pandoc -t plain %pistol-filename%";
+      }
+      {
+        mime = "text/html";
+        command = "pandoc -t plain %pistol-filename%";
+      }
+      {
+        mime = "text/x-tex";
+        command = "pandoc -t plain %pistol-filename%";
+      }
+      {
+        mime = "application/json";
+        command = "bat --paging=never --color=always %pistol-filename%";
+      }
+      {
+        fpath = ".*\.log(.\d\w+)?(.gz)?$";
+        command = "lnav -n %pistol-filename%";
+      }
+      {
+        mime = "text/*";
+        command = "bat --paging=never --color=always %pistol-filename%";
+      }
+      {
+        fpath = ".*\.done$";
+        command = "file -bi %pistol-filename%";
+      }
+      {
+        mime = "image/*";
+        command = "chafa %pistol-filename%";
+      }
+      {
+        mime = "audio/*";
+        command = "ffprobe -hide_banner  %pistol-filename%";
+      }
+      {
+        mime = "video/*";
+        command = "ffprobe -hide_banner  %pistol-filename%";
+      }
+      {
+        mime = "application/*";
+        command = "hexyl %pistol-filename%";
+      }
+    ];
   };
 }
