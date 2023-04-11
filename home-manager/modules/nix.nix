a7e71c0c4f17addf0ebbc41202b4e7d7c6f54296
Alex Sirois <asirois@dimonoff.com>
2023-04-11 09:51:42 -0400

    feat(nix): add helix config dor nix

--- home-manager/modules/nix.nix
+++ home-manager/modules/nix.nix
@@ -2,6 +2,16 @@
   home.packages = with pkgs; [
     alejandra
     comma
+    nil
   ];
   programs.nix-index.enable = true;
+  programs.helix.languages = [
+    {
+      name = "nix";
+      formatter = {
+        command = "alejandra";
+        args = ["--quiet" "-"];
+      };
+    }
+  ];
 }
