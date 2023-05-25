c257d4c029ad825183564cfbd8bbec01dc95073a
Alex Sirois <asirois@dimonoff.com>
2023-05-25 09:41:39 -0400

    feat(web-browser): remove gnome extension from firefox

--- home-manager/modules/web-browser.nix
+++ home-manager/modules/web-browser.nix
@@ -1,13 +1,5 @@
 {pkgs, ...}: {
   programs.firefox = {
     enable = true;
-    package =
-      pkgs.firefox.override
-      {
-        cfg = {
-          # Gnome shell native connector
-          enableGnomeExtensions = true;
-        };
-      };
   };
 }
