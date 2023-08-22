0de11c11e45e6befb1c5b1e888ffecc0bfceb2d2
Alex Sirois <asirois@dimonoff.com>
2023-08-21 22:59:11 -0400

    feat(editor): remove movement in helix insert mode

--- home-manager/modules/editors.nix
+++ home-manager/modules/editors.nix
@@ -10,6 +10,18 @@
           display-inlay-hints = true;
         };
       };
+      keys = {
+        insert = {
+          up = "no_op";
+          down = "no_op";
+          left = "no_op";
+          right = "no_op";
+          pageup = "no_op";
+          pagedown = "no_op";
+          home = "no_op";
+          end = "no_op";
+        };
+      };
     };
     languages.language = [
       {
