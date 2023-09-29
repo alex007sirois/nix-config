ba4718f0d401af65e5a221b5229a6d56c1fa5bda
Alex Sirois <asirois@dimonoff.com>
2023-09-29 16:25:00 -0400

    feat(python): add black lsp

--- home-manager/modules/python.nix
+++ home-manager/modules/python.nix
@@ -3,13 +3,13 @@
     with ps; [
       pylsp-mypy
       pylsp-rope
+      python-lsp-black
       python-lsp-server
       python-lsp-ruff
     ];
 in {
   home.packages = with pkgs; [
     (python311.withPackages python-lsp-packages)
-    black
     ruff
   ];
 }
