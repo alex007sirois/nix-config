8703493a40428b604280ccbc90e372c5f65cff2a
Alex Sirois <asirois@dimonoff.com>
2023-10-16 11:36:57 -0400

    fix(theme): fix gnome dark theme config

--- home-manager/modules/theme.nix
+++ home-manager/modules/theme.nix
@@ -6,14 +6,10 @@
       package = pkgs.gnome.gnome-themes-extra;
     };
     gtk3.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
     gtk4.extraConfig = {
-      Settings = ''
-        gtk-application-prefer-dark-theme=1
-      '';
+      gtk-application-prefer-dark-theme = 1;
     };
   };
   home.sessionVariables.GTK_THEME = "Adwaita:dark";
