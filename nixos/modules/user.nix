7fb801d9510fb7d6d621fa4adba6b577a7da20e4
Alex Sirois <asirois@dimonoff.com>
2024-02-11 03:22:27 -0500

    feat(user): add initial password

--- nixos/modules/user.nix
+++ nixos/modules/user.nix
@@ -1,5 +1,6 @@
 {pkgs, ...}: {
   users.users.alex = {
+    initialHashedPassword = "$y$j9T$hjs9jLYBrMN6nXTIrrfqZ/$QusVq0iXY0wy9I/nlt1X/uNaW5JLdyS7mP0z1uG.8sA";
     isNormalUser = true;
     description = "Alex Sirois";
     extraGroups = [
