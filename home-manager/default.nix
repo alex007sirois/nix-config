bb914ad3901d982555d2dd46e278c0d1e94ff677
Alex Sirois <asirois@dimonoff.com>
2024-02-24 15:44:37 -0500

    refactor(home-manager): use helper function

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -1,6 +1,1 @@
-{lib, ...}:
-with builtins;
-with lib.attrsets; let
-  fileAttrs = lib.attrsets.filterAttrs (name: value: name != "default.nix" && value == "regular") (readDir ./.);
-  files = mapAttrsToList (name: value: ./${name}) fileAttrs;
-in {imports = files;}
+{outputs, ...}: outputs.lib.find.importLocalModules ./.
