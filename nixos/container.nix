9013f88c6dd0373dd0fb2223f8fad5fe933ee8a3
Alex Sirois <asirois@dimonoff.com>
2024-03-11 15:07:17 -0400

    feat(container): add script to enable qemu in docker

--- nixos/container.nix
+++ nixos/container.nix
@@ -1,3 +1,9 @@
-{...}: {
+{pkgs, ...}: let
+  docker-enable-qemu =
+    pkgs.writeShellScriptBin
+    "docker-enable-qemu"
+    "${pkgs.docker}/bin/docker run --privileged --rm tonistiigi/binfmt --install all";
+in {
   virtualisation.docker.enable = true;
+  environment.systemPackages = [docker-enable-qemu];
 }
