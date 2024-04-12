b8be806949e68524ca4e70d4d19f781d53b9f584
Alex Sirois <asirois@dimonoff.com>
2024-04-12 14:50:28 -0400

    feat(doo-laptop): add docker registry to hosts

--- hosts/doo-laptop/docker-registry.nix
+++ hosts/doo-laptop/docker-registry.nix
@@ -1,4 +1,7 @@
 {
+  networking.extraHosts = ''
+    192.168.103.235   intranet.dimonoff.com
+  '';
   virtualisation.docker.daemon.settings = {
     insecure-registries = ["intranet.dimonoff.com:5000"];
   };
