d2d010952fae1b1f8690e36363e01cbcbef73b75
Alex Sirois <asirois@dimonoff.com>
2024-04-24 12:18:33 -0400

    refactor(direnv): use shell script instead of alias to nixify

--- home-manager/direnv.nix
+++ home-manager/direnv.nix
@@ -1,11 +1,23 @@
-{...}: {
+{pkgs, ...}: let
+  nixify =
+    pkgs.writeShellScriptBin "nixify"
+    ''
+      if [ ! -e flake.nix ];
+      then
+        nix flake new -t github:alex007sirois/nix-config .;
+        direnv allow;
+      elif [ ! -e .envrc ];
+      then
+        echo 'use flake' > .envrc;
+        direnv allow;
+      fi
+    '';
+in {
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     config.global.hide_env_diff = true;
   };
 
-  home.shellAliases = {
-    nixify = "if [ ! -e flake.nix ]; then nix flake new -t github:alex007sirois/nix-config .; direnv allow; elif [ ! -e .envrc ]; then echo 'use flake' > .envrc; direnv allow; fi";
-  };
+  home.packages = [nixify];
 }
