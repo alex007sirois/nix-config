ccd68728adbbbf24c4fc276d5c639aca2bf8c419
Alex Sirois <asirois@dimonoff.com>
2024-06-17 09:46:53 -0400

    feat(stylix): add new enable option

--- nixos/stylix.nix
+++ nixos/stylix.nix
@@ -7,6 +7,7 @@
   imports = [stylix.nixosModules.stylix];
 
   stylix = with pkgs; {
+    enable = true;
     # Alternatives: onedark classic-dark material-darker tokyo-city-dark seti
     base16Scheme = lib.mkDefault "${base16-schemes}/share/themes/onedark.yaml";
     image = lib.mkDefault "${pantheon.elementary-wallpapers}/share/backgrounds/Photo of Valley.jpg";
