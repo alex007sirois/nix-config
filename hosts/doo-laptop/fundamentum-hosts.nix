0faa2ab06f7577ff3a3333f14e8923486863987c
Alex Sirois <asirois@dimonoff.com>
2024-08-01 10:19:56 -0400

    feat(fundamentum-hosts): add new hosts

--- hosts/doo-laptop/fundamentum-hosts.nix
+++ hosts/doo-laptop/fundamentum-hosts.nix
@@ -2,6 +2,7 @@
   networking.extraHosts = ''
     127.0.0.1 business-unit.fundamentum.test
     127.0.0.1 devices-oauth.fundamentum.test
+    127.0.0.1 registration.fundamentum.test
     127.0.0.1 devices-gateway.fundamentum.test
     127.0.0.1 hub.fundamentum.test
     127.0.0.1 hub-gateway.fundamentum.test
@@ -9,9 +10,21 @@
     127.0.0.1 positioning.fundamentum.test
     127.0.0.1 provisioning.fundamentum.test
     127.0.0.1 states.fundamentum.test
+    127.0.0.1 mqtt.fundamentum.test
+    127.0.0.1 pubsub-bridge-adapter.fundamentum.test
+    127.0.0.1 spatium.fundamentum.test
+    127.0.0.1 spatium-gateway.fundamentum.test
+    127.0.0.1 spatium-service.fundamentum.test
+    127.0.0.1 transactions.fundamentum.test
+    127.0.0.1 p-laval-app.fundamentum.test
+    127.0.0.1 laval-panel-mock.fundamentum.test
+    127.0.0.1 actions.fundamentum.test
+    127.0.0.1 panels.fundamentum.test
+    127.0.0.1 poseidon-database.fundamentum.test
 
     ::1 business-unit.fundamentum.test
     ::1 devices-oauth.fundamentum.test
+    ::1 registration.fundamentum.test
     ::1 devices-gateway.fundamentum.test
     ::1 hub.fundamentum.test
     ::1 hub-gateway.fundamentum.test
@@ -19,5 +32,16 @@
     ::1 positioning.fundamentum.test
     ::1 provisioning.fundamentum.test
     ::1 states.fundamentum.test
+    ::1 mqtt.fundamentum.test
+    ::1 pubsub-bridge-adapter.fundamentum.test
+    ::1 spatium.fundamentum.test
+    ::1 spatium-gateway.fundamentum.test
+    ::1 spatium-service.fundamentum.test
+    ::1 transactions.fundamentum.test
+    ::1 p-laval-app.fundamentum.test
+    ::1 laval-panel-mock.fundamentum.test
+    ::1 actions.fundamentum.test
+    ::1 panels.fundamentum.test
+    ::1 poseidon-database.fundamentum.test
   '';
 }
