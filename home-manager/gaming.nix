93a0376c56c6c42a380c59478fea00197065db83
Alex Sirois <alex007sirois@gmail.com>
2024-12-14 15:48:40 -0500

    feat(gaming): update retroarch recipe

--- home-manager/gaming.nix
+++ home-manager/gaming.nix
@@ -8,16 +8,19 @@
 in {
   options.modules.gaming.enable = lib.mkEnableOption "Gaming";
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      cartridges
-      heroic
-      (retroarch.override {
-        cores = with libretro; [
+    home.packages = let
+      retroarchCores = cores:
+        with cores; [
           bsnes
           dolphin
           parallel-n64
         ];
-      })
-    ];
+      retroarch = pkgs.retroarch.withCores retroarchCores;
+    in
+      with pkgs; [
+        cartridges
+        heroic
+        retroarch
+      ];
   };
 }
