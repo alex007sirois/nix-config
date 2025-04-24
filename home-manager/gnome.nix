67c6bb3d96cbe9c29dfa1ca8b92c34bf1f32b4de
Alex Sirois <asirois@dimonoff.com>
2025-04-24 08:49:55 -0400

    refactor(stylix): fix warnings

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -2,6 +2,7 @@
   home.packages = with pkgs.gnomeExtensions; [
     bluetooth-battery-meter
   ];
+  stylix.targets.qt.platform = "qtct";
   dconf = {
     enable = true;
     settings = {
--- home-manager/web-browser.nix
+++ home-manager/web-browser.nix
@@ -3,6 +3,7 @@
     tangram
   ];
 
+  stylix.targets.firefox.profileNames = ["default"];
   programs.firefox = {
     enable = true;
   };
