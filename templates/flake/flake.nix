8f34ed5047473635d11f79b6f21cc35458ccb1f1
Alex Sirois <asirois@dimonoff.com>
2025-07-07 08:57:20 -0400

    feat(template): use flake-utils in template

--- templates/flake/flake.nix
+++ templates/flake/flake.nix
@@ -1,24 +1,23 @@
 {
   description = "A basic flake";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = {nixpkgs, ...}: let
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "aarch64-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-  in {
-    devShells = forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = false;
-      };
-    in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [];
-      };
-    });
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: (flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = with pkgs;
+        mkShell {
+          packages = [];
+        };
+    }
+  ));
 }
--- templates/python-pdm/flake.nix
+++ templates/python-pdm/flake.nix
@@ -1,27 +1,26 @@
 {
   description = "A basic flake";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = {nixpkgs, ...}: let
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "aarch64-linux"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-    ];
-  in {
-    devShells = forAllSystems (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = false;
-      };
-    in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          python312
-          pdm
-        ];
-      };
-    });
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }: (flake-utils.lib.eachDefaultSystem (
+    system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = with pkgs;
+        mkShell {
+          packages = [
+            python313
+            pdm
+          ];
+        };
+    }
+  ));
 }
