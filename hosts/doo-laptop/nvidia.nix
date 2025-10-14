82282e300e592c76422f2b06608edff4c6e7c0f7
Alex Sirois <asirois@dimonoff.com>
2025-10-14 10:45:44 -0400

    feat(nvidia): remove nvidia-offload script

--- hosts/doo-laptop/nvidia.nix
+++ hosts/doo-laptop/nvidia.nix
@@ -2,17 +2,7 @@
   pkgs,
   lib,
   ...
-}: let
-  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
-    export __NV_PRIME_RENDER_OFFLOAD=1
-    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
-    export __GLX_VENDOR_LIBRARY_NAME=nvidia
-    export __VK_LAYER_NV_optimus=NVIDIA_only
-    exec "$@"
-  '';
-in {
-  environment.systemPackages = [nvidia-offload];
-
+}: {
   services.xserver.displayManager.sessionCommands = ''
     ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
   '';
