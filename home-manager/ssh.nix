0102c759dd9fc167efc28e4cb299bbec1bb81816
Alex Sirois <asirois@dimonoff.com>
2025-10-20 16:47:26 -0400

    feat(ssh): add ssh config

--- home-manager/ssh.nix
+++ home-manager/ssh.nix
@@ -4,4 +4,18 @@
     ssh-copy-id
     ssh-tools
   ];
+  programs.ssh = {
+    enable = true;
+    enableDefaultConfig = false;
+    matchBlocks."*" = {
+      identityFile = "~/.ssh/default_key";
+      identitiesOnly = true;
+      compression = true;
+      hashKnownHosts = true;
+      controlMaster = "auto";
+      controlPath = "~/.ssh/sockets/ssh-mux-%r@%h:%p.socket";
+      controlPersist = "30m";
+      setEnv.TERM = "xterm-256color";
+    };
+  };
 }
