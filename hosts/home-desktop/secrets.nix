dcfd985030969560863280ff7d0de3bc45507496
Alex Sirois <alex007sirois@gmail.com>
2025-10-20 17:23:40 -0400

    feat(secrets): add secret for home-desktop linux-password

--- hosts/home-desktop/secrets.nix
+++ hosts/home-desktop/secrets.nix
@@ -6,5 +6,9 @@
       hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqsh2Ex7bY1HOh4HdBKcWpqvKySDZ8j/gHkJRdwc/hd";
       localStorageDir = ./. + "/secrets/rekeyed/";
     };
+
+    secrets = {
+      linux-password.rekeyFile = ./secrets/linux-password.age;
+    };
   };
 }
