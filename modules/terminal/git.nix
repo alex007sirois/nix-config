52ebdb4be91c58da33c2f285d9a67a6166afd87b
Alex Sirois <asirois@dimonoff.com>
2025-11-27 15:00:59 -0500

    fix(git): fix git config

--- modules/terminal/git.nix
+++ modules/terminal/git.nix
@@ -18,7 +18,7 @@
         ];
         settings = {
           user = {inherit (config.user) name email;};
-          aliases = {
+          alias = {
             aliases = "config --get-regexp ^alias\.";
             branches = "branch -a --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:blue)(%(committerdate:short)) [%(authorname)]'";
             logs = "log --pretty=format:'%C(auto)%h%C(reset) %C(cyan)%ad%C(auto)%d%C(reset) %s %C(blue)[%cn]%C(reset)' --date=short-local --ext-diff";
@@ -26,52 +26,50 @@
             uncommit = "reset --mixed HEAD~1";
             nuke = "!git reset --hard && git clean -fdx";
           };
-          extraConfig = {
-            branch.sort = "-committerdate";
-            column.ui = "auto";
-            core = {
-              autocrlf = "input";
-              ignorecase = false;
-            };
-            diff = {
-              algorithm = "histogram";
-              colorMoved = "plain";
-              colormovedws = "allow-indentation-change";
-              mnemonicPrefix = "true";
-              renames = "copy";
-            };
-            fetch = {
-              prune = true;
-              pruneTags = true;
-            };
-            help.autocorrect = "prompt";
-            init.defaultBranch = "main";
-            log = {
-              date = "iso";
-            };
-            merge = {
-              conflictstyle = "zdiff3";
-              directoryRenames = true;
-            };
-            pager.difftool = true;
-            pull = {
-              rebase = true;
-            };
-            push = {
-              autoSetupRemote = true;
-              followTags = true;
-            };
-            rebase = {
-              autoSquash = true;
-              autoStash = true;
-              updateRefs = true;
-            };
-            rerere = {
-              autoupdate = true;
-              enabled = true;
-            };
-            tag.sort = "version:refname";
+          branch.sort = "-committerdate";
+          column.ui = "auto";
+          core = {
+            autocrlf = "input";
+            ignorecase = false;
           };
+          diff = {
+            algorithm = "histogram";
+            colorMoved = "plain";
+            colormovedws = "allow-indentation-change";
+            mnemonicPrefix = "true";
+            renames = "copy";
+          };
+          fetch = {
+            prune = true;
+            pruneTags = true;
+          };
+          help.autocorrect = "prompt";
+          init.defaultBranch = "main";
+          log = {
+            date = "iso";
+          };
+          merge = {
+            conflictstyle = "zdiff3";
+            directoryRenames = true;
+          };
+          pager.difftool = true;
+          pull = {
+            rebase = true;
+          };
+          push = {
+            autoSetupRemote = true;
+            followTags = true;
+          };
+          rebase = {
+            autoSquash = true;
+            autoStash = true;
+            updateRefs = true;
+          };
+          rerere = {
+            autoupdate = true;
+            enabled = true;
+          };
+          tag.sort = "version:refname";
         };
       };
       difftastic = {
@@ -82,19 +80,19 @@
         };
         options.background = "dark";
       };
-    };
-    programs.lazygit = {
-      enable = true;
-      settings = {
-        gui = {
-          returnImmediately = true;
-          nerdFontsVersion = "3";
-          timeFormat = "2006-01-02 15:04:05";
-        };
-        git = {
-          branchLogCmd = "git logs --graph";
-          pagers = [{externalDiffCommand = "difft --color=always";}];
-          parseEmoji = true;
+      lazygit = {
+        enable = true;
+        settings = {
+          gui = {
+            returnImmediately = true;
+            nerdFontsVersion = "3";
+            timeFormat = "2006-01-02 15:04:05";
+          };
+          git = {
+            branchLogCmd = "git logs --graph";
+            pagers = [{externalDiffCommand = "difft --color=always";}];
+            parseEmoji = true;
+          };
         };
       };
     };
