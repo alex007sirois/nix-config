a961b23eaf9a67a1462d5622cc496fb03584e80a
Alex Sirois <asirois@dimonoff.com>
2026-01-05 10:28:31 -0500

    fix(audio): downgrade wireplumber to 0.5.12
    NixOS/nixpkgs#475202

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -1,5 +1,5 @@
 {
-  flake.modules.nixos.system = {
+  flake.modules.nixos.system = {pkgs, ...}: {
     security.rtkit.enable = true;
     services.pulseaudio.enable = false;
     services.pipewire = {
@@ -7,6 +7,14 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+
+      wireplumber.package = pkgs.wireplumber.overrideAttrs (prev: {
+        version = "0.5.12";
+        src = prev.src.override {
+          rev = "0.5.12";
+          hash = "sha256-3LdERBiPXal+OF7tgguJcVXrqycBSmD3psFzn4z5krY=";
+        };
+      });
     };
   };
 }
