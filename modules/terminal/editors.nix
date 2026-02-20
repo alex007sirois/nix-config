25443f2a6cb59b21b92b9865e1690837f2f26dd9
Alex Sirois <asirois@dimonoff.com>
2026-02-20 11:54:08 -0500

    feat(editor): configure helix for React

--- modules/terminal/editors.nix
+++ modules/terminal/editors.nix
@@ -1,164 +1,188 @@
 {
-  flake.modules.homeManager.terminal = {pkgs, ...}: {
-    home.packages = with pkgs; [
-      basedpyright
-      ruff
-      bash-language-server
-      buf
-      dockerfile-language-server
-      gopls
-      vscode-langservers-extracted
-      taplo
-      yaml-language-server
+  flake.modules.homeManager.terminal =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        basedpyright
+        ruff
+        bash-language-server
+        buf
+        dockerfile-language-server
+        gopls
+        vscode-langservers-extracted
+        taplo
+        yaml-language-server
 
         nixfmt
         nixfmt-tree
         nil
         nixd
 
-      tailwindcss-language-server
-      vue-language-server
-      typescript-language-server
-      prettier
-    ];
-
-    programs.helix = {
-      enable = true;
-      defaultEditor = true;
-      ignores = [
-        "!.dockerignore"
-        "!.env*"
-        "!.gitlint"
-        "!.helix"
-        "!.helm"
-        "!*.toml"
-        "!*.ya?ml"
+        typescript-language-server
       ];
-      settings = {
-        theme = "dark_plus";
-        editor = {
-          bufferline = "multiple";
-          color-modes = true;
-          cursorline = true;
-          cursor-shape = {
-            insert = "bar";
-            normal = "block";
-            select = "underline";
-          };
-          end-of-line-diagnostics = "hint";
-          inline-diagnostics.cursor-line = "warning";
-          indent-guides.render = true;
-          lsp = {
-            display-messages = true;
-            display-inlay-hints = true;
-          };
-        };
-        keys = {
-          normal.X = ["extend_line_up" "extend_to_line_bounds"];
-          select.X = ["extend_line_up" "extend_to_line_bounds"];
-          insert = {
-            up = "no_op";
-            down = "no_op";
-            left = "no_op";
-            right = "no_op";
-            pageup = "no_op";
-            pagedown = "no_op";
-            home = "no_op";
-            end = "no_op";
-          };
-        };
-      };
-      languages = {
-        language-server = {
-          rust-analyzer = {
-            config.rust-analyzer = {
-              check.command = "clippy";
-              imports = {
-                granularity.group = "module";
-                prefix = "self";
-              };
-            };
-          };
-          basedpyright.config.basedpyright = {
-            disableOrganizeImports = true;
-            analysis = {
-              diagnosticMode = "workspace";
-              diagnosticSeverityOverrides = let
-                disabled = "none";
-                ruff = disabled;
-                info = "information";
-                warning = "warning";
-                unreachable = "unreachable";
-                deprecated = "deprecated";
-                unused = "unused";
-              in {
-                reportCallInDefaultInitializer = ruff;
-                reportPrivateUsage = ruff;
-                reportUnusedImport = ruff;
-                reportUnusedVariable = ruff;
-                reportUnusedParameter = ruff;
-                reportUnknownMemberType = disabled;
-                reportUnusedCallResult = disabled;
-                reportUnreachable = unreachable;
-                reportUnusedClass = unused;
-                reportUnusedFunction = unused;
-                reportDeprecated = deprecated;
-                reportInvalidCast = info;
-                reportMissingModuleSource = warning;
-                reportMissingTypeStubs = warning;
-                reportPrivateLocalImportUsage = warning;
-                reportUnknownArgumentType = warning;
-                reportUnknownLambdaType = warning;
-                reportUnknownParameterType = warning;
-                reportUnknownVariableType = warning;
-              };
-            };
-          };
-          vuels.config = {
-            vue.hybridMode = true;
-            typescript.tsdk = "${pkgs.vue-language-server}/lib/language-tools/node_modules/typescript/lib";
-          };
-          typescript-language-server.config = {
-            plugins = [
-              {
-                name = "@vue/typescript-plugin";
-                location = "${pkgs.vue-language-server}/bin/vue-language-server";
-                languages = ["vue"];
-              }
-            ];
-            tsserver.path = "${pkgs.vue-language-server}/lib/language-tools/node_modules/typescript/lib";
-            vue.inlayHints = {
-              includeInlayEnumMemberValueHints = true;
-              includeInlayFunctionLikeReturnTypeHints = true;
-              includeInlayFunctionParameterTypeHints = true;
-              includeInlayParameterNameHints = "all";
-              includeInlayParameterNameHintsWhenArgumentMatchesName = true;
-              includeInlayPropertyDeclarationTypeHints = true;
-              includeInlayVariableTypeHints = true;
-            };
-          };
-        };
-        language = [
-          {
-            name = "nix";
-            auto-format = true;
-          }
-          {
-            name = "python";
-            language-servers = ["basedpyright" "ruff"];
-            auto-format = true;
-          }
-          {
-            name = "vue";
-            auto-format = true;
-            formatter = {
-              command = "prettier";
-              args = ["--parser" "vue"];
-            };
-            language-servers = ["vuels" "typescript-language-server" "tailwindcss-ls"];
-          }
+
+      programs.helix = {
+        enable = true;
+        defaultEditor = true;
+        ignores = [
+          "!.dockerignore"
+          "!.env*"
+          "!.gitlint"
+          "!.helix"
+          "!.helm"
+          "!*.toml"
+          "!*.ya?ml"
         ];
+        settings = {
+          theme = "dark_plus";
+          editor = {
+            bufferline = "multiple";
+            color-modes = true;
+            cursorline = true;
+            cursor-shape = {
+              insert = "bar";
+              normal = "block";
+              select = "underline";
+            };
+            end-of-line-diagnostics = "hint";
+            inline-diagnostics.cursor-line = "warning";
+            indent-guides.render = true;
+            lsp = {
+              display-messages = true;
+              display-inlay-hints = true;
+            };
+          };
+          keys = {
+            normal.X = [
+              "extend_line_up"
+              "extend_to_line_bounds"
+            ];
+            select.X = [
+              "extend_line_up"
+              "extend_to_line_bounds"
+            ];
+            insert = {
+              up = "no_op";
+              down = "no_op";
+              left = "no_op";
+              right = "no_op";
+              pageup = "no_op";
+              pagedown = "no_op";
+              home = "no_op";
+              end = "no_op";
+            };
+          };
+        };
+        languages = {
+          language-server = {
+            rust-analyzer = {
+              config.rust-analyzer = {
+                check.command = "clippy";
+                imports = {
+                  granularity.group = "module";
+                  prefix = "self";
+                };
+              };
+            };
+            basedpyright.config.basedpyright = {
+              disableOrganizeImports = true;
+              analysis = {
+                diagnosticMode = "workspace";
+                diagnosticSeverityOverrides =
+                  let
+                    disabled = "none";
+                    ruff = disabled;
+                    info = "information";
+                    warning = "warning";
+                    unreachable = "unreachable";
+                    deprecated = "deprecated";
+                    unused = "unused";
+                  in
+                  {
+                    reportCallInDefaultInitializer = ruff;
+                    reportPrivateUsage = ruff;
+                    reportUnusedImport = ruff;
+                    reportUnusedVariable = ruff;
+                    reportUnusedParameter = ruff;
+                    reportUnknownMemberType = disabled;
+                    reportUnusedCallResult = disabled;
+                    reportUnreachable = unreachable;
+                    reportUnusedClass = unused;
+                    reportUnusedFunction = unused;
+                    reportDeprecated = deprecated;
+                    reportInvalidCast = info;
+                    reportMissingModuleSource = warning;
+                    reportMissingTypeStubs = warning;
+                    reportPrivateLocalImportUsage = warning;
+                    reportUnknownArgumentType = warning;
+                    reportUnknownLambdaType = warning;
+                    reportUnknownParameterType = warning;
+                    reportUnknownVariableType = warning;
+                  };
+              };
+            };
+            typescript-language-server.config = {
+              hostInfo = "helix";
+              documentFormatting = false;
+              completions.completeFunctionCalls = true;
+              typescript.inlayHints = {
+                includeInlayEnumMemberValueHints = true;
+                includeInlayFunctionLikeReturnTypeHints = true;
+                includeInlayFunctionParameterTypeHints = true;
+                includeInlayParameterNameHints = "all";
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                includeInlayPropertyDeclarationTypeHints = true;
+                includeInlayVariableTypeHints = true;
+              };
+            };
+            vscode-eslint-language-server.config = {
+              format = true;
+              onIgnoredFiles = "off";
+              packageManager = "pnpm";
+              quiet = false;
+              useESLintClass = false;
+              codeActionOnSave.mode = "all";
+            };
+          };
+          language = [
+            {
+              name = "nix";
+              auto-format = true;
+            }
+            {
+              name = "python";
+              language-servers = [
+                "basedpyright"
+                "ruff"
+              ];
+              auto-format = true;
+            }
+          ]
+          ++
+            map
+              (name: {
+                inherit name;
+                language-servers = [
+                  "typescript-language-server"
+                  "vscode-eslint-language-server"
+                ];
+                auto-format = true;
+                formatter = {
+                  command = "npx";
+                  args = [
+                    "prettier"
+                    "--parser"
+                    "typescript"
+                  ];
+                };
+              })
+              [
+                "javascript"
+                "jsx"
+                "typescript"
+                "tsx"
+              ];
+        };
       };
     };
-  };
 }
