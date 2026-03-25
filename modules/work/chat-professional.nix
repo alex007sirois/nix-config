cf5a534f77d1a7bf9e7831ff7b031f7fdd6a8960
Alex Sirois <asirois@dimonoff.com>
2026-03-25 08:39:51 -0400

    feat(work): add separate teams entries

--- modules/work/chat-professional.nix
+++ modules/work/chat-professional.nix
@@ -5,5 +5,19 @@
       home.packages = with pkgs; [
         teams-for-linux
       ];
+      xdg.desktopEntries = {
+        teams-dimonoff = {
+          name = "Teams Dimonoff";
+          icon = "teams-for-linux";
+          exec = "teams-for-linux --user-data-dir==/home/alex/.config/teams-dimonoff";
+          terminal = false;
+        };
+        teams-stelpro = {
+          name = "Teams Stelpro";
+          icon = "teams-for-linux";
+          exec = "teams-for-linux --user-data-dir==/home/alex/.config/teams-stelpro";
+          terminal = false;
+        };
+      };
     };
 }
