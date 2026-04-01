e9d5185171323d85377700c70665c375532cc0aa
Alex Sirois <asirois@dimonoff.com>
2026-04-01 11:21:15 -0400

    build(nix): update lock file

--- flake.nix
+++ flake.nix
@@ -1,54 +1,51 @@
 # DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
 # Use `nix run .#write-flake` to regenerate it.
 {
-
   outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./modules);
 
   inputs = {
     agenix = {
+      url = "github:ryantm/agenix";
       inputs = {
         home-manager.follows = "home-manager";
         nixpkgs.follows = "nixpkgs";
       };
-      url = "github:ryantm/agenix";
     };
     agenix-rekey = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:oddlama/agenix-rekey";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     bitdefender-nix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "git+https://bitbucket.org/amotus/bitdefender-nix.git";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-file.url = "github:vic/flake-file";
     flake-parts = {
-      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs-lib";
     };
     home-manager = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
     nix-index-database = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:Mic92/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-lib.follows = "nixpkgs";
     stylix = {
-      inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/stylix";
-    };
-    systems.url = "github:nix-systems/default";
-    tactical-rmm = {
       inputs.nixpkgs.follows = "nixpkgs";
+    };
+    tactical-rmm = {
       url = "git+https://bitbucket.org/amotus/tactical-rmm-nix.git";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
 }
