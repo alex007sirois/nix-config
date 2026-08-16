3b884a9f61c2c780d96ee38940d18e5888a68a72
Alex Sirois <alex007sirois@gmail.com>
2026-08-16 12:59:21 -0400

    feat(pi-server): use new agenix key

--- modules/hosts/pi-server/secrets.nix
+++ modules/hosts/pi-server/secrets.nix
@@ -2,7 +2,7 @@
   flake.modules =
     let
       identityPaths = [ "/home/alex/.ssh/agenix_key" ];
-      rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKqsh2Ex7bY1HOh4HdBKcWpqvKySDZ8j/gHkJRdwc/hd";
+      rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOJL5dI6K0Yb8TIm6y9G1fGe8c6Bs+QynOg0TTwrXRg";
     in
     {
       homeManager.pi-server.age = { inherit identityPaths rekey; };
