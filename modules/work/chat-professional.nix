a755bd4ced299a5a27d0cbc494abc574c9d6952a
Alex Sirois <asirois@dimonoff.com>
2026-08-25 10:14:31 -0400

    fix(work): fix teams config path

--- modules/work/chat-professional.nix
+++ modules/work/chat-professional.nix
@@ -7,15 +7,15 @@
       ];
       xdg.desktopEntries = {
         teams-dimonoff = {
-          name = "Teams Dimonoff";
+          name = "Teams Vectanor";
           icon = "teams-for-linux";
-          exec = "teams-for-linux --user-data-dir==/home/alex/.config/teams-dimonoff";
+          exec = "teams-for-linux --user-data-dir=/home/alex/.config/teams-vectanor";
           terminal = false;
         };
         teams-stelpro = {
           name = "Teams Stelpro";
           icon = "teams-for-linux";
-          exec = "teams-for-linux --user-data-dir==/home/alex/.config/teams-stelpro";
+          exec = "teams-for-linux --user-data-dir=/home/alex/.config/teams-stelpro";
           terminal = false;
         };
       };
