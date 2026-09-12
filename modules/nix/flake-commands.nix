bf8acb299d81fbfdc340662d5302465e28be5a08
Alex Sirois <alex007sirois@gmail.com>
2026-09-12 10:06:06 -0400

    feat(nix): experimental-features now a list

--- modules/nix/flake-commands.nix
+++ modules/nix/flake-commands.nix
@@ -17,7 +17,7 @@
         nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
         settings = {
-          experimental-features = "nix-command flakes";
+          experimental-features = [ "nix-command flakes" ];
           auto-optimise-store = true;
         };
       };
