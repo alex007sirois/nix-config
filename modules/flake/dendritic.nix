8441bd61ba29a2c379365c4d7484cfbd896d966d
Alex Sirois <alex007sirois@gmail.com>
2026-09-12 10:11:40 -0400

    feat(nix): use new org

--- modules/flake/dendritic.nix
+++ modules/flake/dendritic.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  flake-file.inputs.flake-file.url = "github:vic/flake-file";
+  flake-file.inputs.flake-file.url = "github:denful/flake-file";
   imports = with inputs.flake-file.flakeModules; [
     dendritic
   ];
