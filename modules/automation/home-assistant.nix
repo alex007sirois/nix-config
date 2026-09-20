c0dc7b243467cc515570547dcbab7fea077ab7dd
Alex Sirois <alex007sirois@gmail.com>
2026-09-20 00:58:49 -0400

    feat(automation): update home-assistant

--- modules/automation/home-assistant.nix
+++ modules/automation/home-assistant.nix
@@ -4,7 +4,7 @@
       networking.firewall.allowedTCPPorts = [ 8123 ];
       virtualisation.oci-containers.containers = {
         homeassistant = {
-          image = "ghcr.io/home-assistant/home-assistant:2025.12.3";
+          image = "ghcr.io/home-assistant/home-assistant:2026.9";
           pull = "newer";
           volumes = [ "home-assistant:/config" ];
           environment.TZ = "America/Montreal";
